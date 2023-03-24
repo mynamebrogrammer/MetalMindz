cf1f44b69ce24d559f9a3b410f9347032693712a
TyGosley <tygosley@gmail.com>
2023-03-23 20:34:34 -0700

    postadata and seed.json code in progress

--- Main/seeds/seed.js
+++ Main/seeds/seed.js
@@ -1,9 +1,36 @@
+// const sequelize = require('../config/connection');
+// const { User, Robot, Post } = require('../models');
+
+// const userData = require('./userData.json');
+// const postData = require('./postData.json');
+// const robotData = require('./userData.json');
+
+// const seedDatabase = async () => {
+//   await sequelize.sync({ force: true });
+
+//   const users = await User.bulkCreate(userData, {
+//     individualHooks: true,
+//     returning: true,
+//   });
+
+//   for (const post of postData) {
+//     await Post.create({
+//       ...post,
+//       user_id: users[Math.floor(Math.random() * users.length)].id,
+//     });
+//   }
+
+//   process.exit(0);
+// };
+
+// seedDatabase();
+
 const sequelize = require('../config/connection');
 const { User, Robot, Post } = require('../models');
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
-const robotData = require('./userData.json');
+const robotData = require('./robotData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,10 +40,18 @@ const seedDatabase = async () => {
     returning: true,
   });
 
+  const robots = await Robot.bulkCreate(robotData, {
+    returning: true,
+  });
+
   for (const post of postData) {
+    const user = users[Math.floor(Math.random() * users.length)];
+    const robot = robots[Math.floor(Math.random() * robots.length)];
+
     await Post.create({
       ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+      user_id: user.id,
+      robot_id: robot.id,
     });
   }
 
