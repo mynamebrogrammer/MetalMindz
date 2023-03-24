d1376cec73b56703e4bd9046ab335850ccb2eabd
Habib Maksoud <habibmaksoud@gmail.com>
2023-03-23 19:29:23 -0700

    merge conflicts and fixed them

---
+++ Main/models/Robot.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Robot extends Model {}
+
+Robot.init(
+ {
+    id: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+         primaryKey: true,
+         autoIncrement: true,
+          },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+      image: {
+        type: DataTypes.STRING,
+        defaultValue: '#',
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'robot',
+    }
+  );
+  module.exports = Robot;
+  
