ad14cdb99d90da69f2252b369dea37b2f729646c
TyGosley <tygosley@gmail.com>
2023-03-23 18:39:46 -0700

    created Robot.js in models , added queries in index.js

--- Main/models/index.js
+++ Main/models/index.js
@@ -1,10 +1,17 @@
 const User = require('./User');
 const Post = require('./Post');
+const Robot = require('./Robot');
 
 // Future Dev
 // const like
 // const comment
 // const follower
+
+User.belongsToMany(Robot, { through: 'UserRobot' });
+Robot.belongsToMany(User, { through: 'UserRobot' });
+
+// Robot.belongsToMany(User, { foreignKey: 'user_id' });
+// Robot.belongsToMany(Image, { foreignKey: 'image_id' });
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
@@ -15,4 +22,4 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+module.exports = { User, Robot, Post };
