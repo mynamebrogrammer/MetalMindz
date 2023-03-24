4f58caf464daf4fcd1932461a27dff27d077d2e2
Habib Maksoud <habibmaksoud@gmail.com>
2023-03-23 19:09:47 -0700

    updated postsRoutes, projectRoutes, homeRoutes, index.js

--- Main/controllers/api/index.js
+++ Main/controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const robotRoutes = require('./robotRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/robots', robotRoutes);
 
 module.exports = router;
